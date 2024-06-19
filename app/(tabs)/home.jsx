e0665e89724b9be7a0a91e25ac3342d17fec8fa9
AmilSrinath <amilsrinath5@gmail.com>
2024-06-19 15:07:49 +0530

    Silder added for home form

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,14 +1,17 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import Header from '../../components/Home/Header'
+import Slider from '../../components/Home/Slider'
 
 export default function home() {
   return (
     <View>
       {/* Header */}
       <Header />
-      {/* Slider */}
 
+      {/* Slider */}
+      <Slider />
+      
       {/* Category */}
 
       {/* Popular Bussiness List */}
